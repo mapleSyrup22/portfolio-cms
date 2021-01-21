c45b3b1444f121bb8264323df40acda4fec07a6d
ramilamparo <ramilamparo@gmail.com>
2021-01-21 10:05:18 +0400

    Add production database.

--- config/database.js
+++ config/database.js
@@ -1,15 +1,35 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+module.exports = ({ env }) => {
+	if (env("NODE_ENV") !== "production") {
+		return {
+			defaultConnection: "default",
+			connections: {
+				default: {
+					connector: "bookshelf",
+					settings: {
+						client: "sqlite",
+						filename: env("DATABASE_FILENAME", ".tmp/data.db")
+					},
+					options: {
+						useNullAsDefault: true
+					}
+				}
+			}
+		};
+	}
+	return {
+		defaultConnection: "default",
+		connections: {
+			default: {
+				connector: "bookshelf",
+				settings: {
+					client: "mysql",
+					port: 3306,
+					host: env("DATABASE_HOST"),
+					database: env("DATABASE_NAME"),
+					username: env("DATABASE_USER"),
+					password: env("DATABASE_PASS")
+				}
+			}
+		}
+	};
+};
